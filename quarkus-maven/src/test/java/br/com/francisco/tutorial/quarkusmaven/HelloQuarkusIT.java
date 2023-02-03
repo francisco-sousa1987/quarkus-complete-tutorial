c496541a4e205f0ad65e164416e2910c594dacbd
Francisco Sousa <francisco.sousa1987@outlook.com>
2023-02-03 14:34:07 -0300

    create a Quarkus project through the Maven

---
+++ quarkus-maven/src/test/java/br/com/francisco/tutorial/quarkusmaven/HelloQuarkusIT.java
@@ -0,0 +1,8 @@
+package br.com.francisco.tutorial.quarkusmaven;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloQuarkusIT extends HelloQuarkusTest {
+    // Execute the same tests but in packaged mode.
+}
